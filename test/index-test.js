efd2c3b2e3f4cfc5dd295b4fec28ac119c68d05c
sami4thewin <samiomarmohammed@gmail.com>
2017-03-22 08:48:15 -0400

    Done

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,15 @@
+// const chai = require('chai')
+// const supertest = require('supertest')
+// /* ADD ME! */
+// const app = require('../app')
+//
+// const expect = chai.expect
+//
+// describe('app', function() {
+//   it('runs')
+// })
+
+
 const chai = require('chai')
 const supertest = require('supertest')
 /* ADD ME! */
@@ -5,6 +17,45 @@ const app = require('../app')
 
 const expect = chai.expect
 
-describe('app', function() {
-  it('runs')
+let server
+
+before(done => {
+  return app.up().then(_server => {
+    server = _server
+    done()
+  })
+})
+
+after(() => {
+  server.close()
+})
+
+describe('app', () => {
+  describe('up', () => {
+    it('is a function', () => {
+      expect(app.up).to.be.an.instanceof(Function)
+    })
+  })
+
+  describe('/user', () => {
+    describe('POST', () => {
+      it('fails with an empty request body', done => {
+        supertest(server).
+          post('/user').
+          expect(400, done)
+      })
+
+      it('succeeds with valid name, username, and email', done => {
+        supertest(server).
+          post('/user').
+          send({
+            email: 'test@email.com',
+            name: 'testName',
+            username: 'testUsername'
+          }).
+          set('content-type', 'application/json').
+          expect(200, done)
+      })
+    })
+  })
 })
